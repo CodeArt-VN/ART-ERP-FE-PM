f9ff90d605ebc17e597846e98bc15856f3c3af2d
Hùng Vũ <hungvq@live.com>
2024-05-28 09:30:44 +0700

    clean

--- routing.module.ts
+++ routing.module.ts
@@ -3,8 +3,8 @@ import { AuthGuard } from 'src/app/guards/app.guard';
 
 export const PMRoutes: Routes = [
     
-    { path: 'gantt', loadChildren: () => import('./gantt/gantt.module').then(m => m.GanttPageModule) },
-    { path: 'task', loadChildren: () => import('./task/task.module').then(m => m.TaskPageModule), canActivate: [AuthGuard] },
+    // { path: 'gantt', loadChildren: () => import('./gantt/gantt.module').then(m => m.GanttPageModule) },
+    // { path: 'task', loadChildren: () => import('./task/task.module').then(m => m.TaskPageModule), canActivate: [AuthGuard] },
 
-    { path: 'task-todo', loadChildren: () => import('./task-todo/task-todo.module').then(m => m.TaskTodoPageModule), canActivate: [AuthGuard] },
+    // { path: 'task-todo', loadChildren: () => import('./task-todo/task-todo.module').then(m => m.TaskTodoPageModule), canActivate: [AuthGuard] },
 ];
