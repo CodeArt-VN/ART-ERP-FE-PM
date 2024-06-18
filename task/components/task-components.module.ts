68b97fc90af79296346b97c2ec32d4f567b91199
thienvo <111thienvo@gmail.com>
2024-06-18 14:05:56 +0700

    update task

---
+++ task/components/task-components.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { ShareModule } from 'src/app/share.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { GanttComponent } from './gantt/gantt.component';
+import { BoardComponent } from './board/board.component';
+import { DragDropModule, CdkDropListGroup, CdkDropList, CdkDrag } from '@angular/cdk/drag-drop';
+
+@NgModule({
+  imports: [
+    IonicModule,
+    CommonModule,
+    ShareModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CdkDropListGroup,
+    CdkDropList,
+    CdkDrag,
+    DragDropModule,
+  ],
+  declarations: [GanttComponent, BoardComponent],
+  exports: [GanttComponent, BoardComponent],
+})
+export class TaskComponentsModule {}
